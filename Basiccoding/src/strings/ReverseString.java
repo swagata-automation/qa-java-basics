8c5b36501e566905979c3d958f8e3b860894b6b3
Swaga <Swaga@Swagata>
2026-05-31 07:40:01 +0530

    Add Reverse string coding

---
+++ Basiccoding/src/strings/ReverseString.java
@@ -0,0 +1,16 @@
+package strings;
+
+public class ReverseString {
+
+    public static void main(String[] args) {
+
+        String input = "Hello";
+        String reversed = "";
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
+        }
+
+        System.out.println("Reversed string is: " + reversed);
+    }
+}
