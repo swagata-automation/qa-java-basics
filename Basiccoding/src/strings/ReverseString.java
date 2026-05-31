f0f5a0cef3477e22a32f7aa407ad0f9f466bf9b5
Swaga <Swaga@Swagata>
2026-05-31 07:47:00 +0530

    String Coding

--- Basiccoding/src/strings/ReverseString.java
+++ Basiccoding/src/strings/ReverseString.java
@@ -9,6 +9,7 @@ public class ReverseString {
 
         for (int i = input.length() - 1; i >= 0; i--) {
             reversed = reversed + input.charAt(i);
+            
         }
 
         System.out.println("Reversed string is: " + reversed);
